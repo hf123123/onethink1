a58326d5bef8d96bc46ab95ebcb3109953ec2f69
flyok666 <flyok666@qq.com>
2017-07-18 16:38:32 +0800

    品牌管理

--- backend/controllers/BrandController.php
+++ backend/controllers/BrandController.php
@@ -3,6 +3,8 @@
 namespace backend\controllers;
 
 use backend\models\Brand;
+use yii\data\Pagination;
+use yii\web\NotFoundHttpException;
 
 class BrandController extends \yii\web\Controller
 {
@@ -10,18 +12,46 @@ class BrandController extends \yii\web\Controller
     public function actionAdd()
     {
         $model = new Brand();
-
+        if($model->load(\Yii::$app->request->post()) && $model->validate()){
+            $model->save();
+            \Yii::$app->session->setFlash('success','品牌添加成功');
+            return $this->redirect(['brand/index']);
+        }
         return $this->render('add',['model'=>$model]);
     }
 
     //修改品牌
     public function actionEdit($id){
-        //
+        $model = Brand::findOne(['id'=>$id]);
+        if($model==null){//如果品牌不存在，则显示404页面
+            throw new NotFoundHttpException('品牌不存在');
+        }
+        if($model->load(\Yii::$app->request->post()) && $model->validate()){
+            $model->save();
+            \Yii::$app->session->setFlash('success','品牌添加成功');
+            return $this->redirect(['brand/index']);
+        }
+        return $this->render('add',['model'=>$model]);
     }
-
+    //品牌列表
     public function actionIndex()
     {
-        return $this->render('index');
+        //只显示未删除的品牌，status != -1
+        $query = Brand::find()->where(['!=','status','-1']);
+        $pager = new Pagination([
+            'totalCount'=>$query->count(),
+            'defaultPageSize'=>10,
+        ]);
+        $models = $query->limit($pager->limit)->offset($pager->offset)->all();
+        return $this->render('index',['models'=>$models,'pager'=>$pager]);
     }
-
+    //删除品牌
+    public function actionDel($id){
+        $model = Brand::findOne(['id'=>$id]);
+        //逻辑删除，只修改状态，不真实删除数据
+        if($model){
+            $model->status = -1;
+            $model->save();
+        }
+    }
 }
--- backend/views/brand/index.php
+++ backend/views/brand/index.php
@@ -1,9 +1,21 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>brand/index</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<h1>品牌列表</h1>
+<table class="table table-bordered table-responsive">
+    <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>LOGO</th>
+        <th>操作</th>
+    </tr>
+    <?php foreach($models as $model):?>
+    <tr>
+        <td><?=$model->id?></td>
+        <td><?=$model->name?></td>
+        <td><?=$model->logo?></td>
+        <td><?=\yii\bootstrap\Html::a('修改',['brand/edit','id'=>$model->id])?>
+            <?=\yii\bootstrap\Html::a('删除',['brand/del','id'=>$model->id])?></td>
+    </tr>
+    <?php endforeach;?>
+</table>
