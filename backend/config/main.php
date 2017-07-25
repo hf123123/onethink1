46fcb04af12980382cf8242f594faaf47063c975
flyok666 <flyok666@qq.com>
2017-07-25 14:35:58 +0800

    用户管理 用户登录

--- backend/config/main.php
+++ backend/config/main.php
@@ -18,7 +18,8 @@ return [
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
-            'identityClass' => 'common\models\User',
+            'identityClass' => \backend\models\User::className(),
+            'loginUrl'=>['user/login'],
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
